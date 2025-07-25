bc84b96c28852ccc303c891bb4d0f21a5cb4484c
elcin-max <dani17elc@gmail.com>
2025-07-25 21:52:44 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+import expres from "express";
+import cors from "cors";
+import  fs from "fs/promises";
+import path from "path";
+
+
+const app = expres();
+app.use(cors());
+app.use(expres.json()); 
+
+app.post("/create_folder", (req, res)=> {
+    const foldername = req.body.foldername;
+})
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
